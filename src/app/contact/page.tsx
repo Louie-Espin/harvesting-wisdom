04fe2b6f4fca3eb8922d1091606d20761310136c
Louie-Espin <luisespinoza112599@gmail.com>
2024-01-25 10:19:33 -0700

    Updated contact page.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,11 @@
 export default function Contact() {
     return (
-        <main className='w-full h-[70vh] flex items-center justify-center'>
-            <h1 className='font-serif font-semibold text-3xl'>Coming Soon!</h1>
+        <main className='w-full h-[60vh] flex flex-col items-center justify-center'>
+            <h1 className='font-serif font-semibold text-3xl mb-4'>Stay in Touch.</h1>
+            <p>Dr. Joe Roselle</p>
+            <p>Chief Operating Officer</p>
+            <a href='tel:4802053590' className='underline'>+1 (480) 205-3590</a>
+            <a href='mailto:jroselle@weareufe.org' className='underline'>jroselle@weareufe.org</a>
         </main>
     );
 }
